78c672ec7dbf10dc0b6ccd6ca4c2124f2de2ef63
Daniel Freitas <daniel.italofreitas@gmail.com>
2021-05-22 02:34:24 -0300

    feat: list, find and remove user

--- vacinacao-app/vacinacao-front/src/app/user.service.ts
+++ vacinacao-app/vacinacao-front/src/app/user.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from './user/user.interface';
 import { HttpClient } from '@angular/common/http';
+import { AbstractControl } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
   API_URL = 'http://localhost:8080/user';
   constructor(private http: HttpClient) { }
 
@@ -18,12 +20,16 @@ export class UserService {
     return this.http.get<User[]>(this.API_URL);
   }
 
-  removerUser(id: number) {
+  removeUser(id: number) {
     return this.http.delete<User>(this.API_URL + '/' + id);
   }
 
   updateUser(newUser: User) {
     return this.http.put<User>(this.API_URL, newUser);
+  }
+
+  findById(id: number) {
+    return this.http.get<User>(this.API_URL + '/' + id);
   }
 
 }
--- vacinacao-app/vacinacao-front/src/app/user/user.component.ts
+++ vacinacao-app/vacinacao-front/src/app/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+import { User } from './user.interface';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  form = this.formBuilder.group({
+    searchParameter: [null]
+  })
+
+  users: User[] = [];
+
+  constructor(
+    private route: Router,
+    private userService: UserService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.getAllUser();
+  }
+
+  add() {
+    this.route.navigateByUrl('/add-user');
+  }
+
+  getAllUser() {
+    this.userService.getAllUser().subscribe((users) => {
+      this.users = users;
+    });
+  }
+
+  remover(user: User) {
+    console.log(user);
+    if (user.id) {
+      this.userService.removeUser(user.id).subscribe(success => {
+        this.getAllUser();
+      });
+    }
+  }
+
+  findById() {
+    const id = this.form.controls.searchParameter.value;
+
+    if (id) {
+      this.userService.findById(id).subscribe((user: User) => {
+        const tempUsers = [user];
+        this.users = tempUsers;
+      }, (error) => {
+        this.users = [];
+      })
+    } else {
+      this.getAllUser();
+    }
+
   }
 
 }
